f90ba3241f83d9e5558170f497b9af0385b6641f
Sriharish <sriharish.v@gmail.com>
2019-04-20 20:23:29 -0500

    bottom-sheet example

--- src/app/components/bottom-sheet/bottom-sheet.component.ts
+++ src/app/components/bottom-sheet/bottom-sheet.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatBottomSheet, MatBottomSheetRef} from '@angular/material';
 
+/**
+ * @title Bottom Sheet Overview
+ */
 @Component({
   selector: 'app-bottom-sheet',
   templateUrl: './bottom-sheet.component.html',
-  styleUrls: ['./bottom-sheet.component.scss']
+  styleUrls: ['./bottom-sheet.component.scss'],
 })
-export class BottomSheetComponent implements OnInit {
+export class BottomSheetComponent {
+  constructor(private bottomSheet: MatBottomSheet) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  openBottomSheet(): void {
+    this.bottomSheet.open(BottomSheetOverviewExampleSheet);
   }
+}
 
+@Component({
+  selector: 'bottom-sheet-overview-example-sheet',
+  templateUrl: 'bottom-sheet-overview-example-sheet.html',
+})
+export class BottomSheetOverviewExampleSheet {
+  constructor(private bottomSheetRef: MatBottomSheetRef<BottomSheetOverviewExampleSheet>) {}
+
+  openLink(event: MouseEvent): void {
+    this.bottomSheetRef.dismiss();
+    event.preventDefault();
+  }
 }
--- src/app/components/bottom-sheet/bottom-sheet.module.ts
+++ src/app/components/bottom-sheet/bottom-sheet.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../../material.module';
 import { BottomSheetComponent } from './bottom-sheet.component';
 import { RouterModule, Routes } from '@angular/router';
+import { BottomSheetOverviewExampleSheet } from './bottom-sheet.component';
 
 const routes: Routes = [
   {
@@ -12,11 +13,14 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [BottomSheetComponent],
+  declarations: [BottomSheetComponent, BottomSheetOverviewExampleSheet],
   imports: [
     CommonModule,
     MaterialModule,
     RouterModule.forChild(routes)
+  ],
+  entryComponents: [
+    BottomSheetOverviewExampleSheet
   ]
 })
 export class BottomSheetModule { }
